baada0b71e29cc38543421b7e618b0f5487c8319
Kelsi <kelsidj@gmail.com>
2019-02-27 12:44:39 -0600

    Update Cars.java

--- src/model/Cars.java
+++ src/model/Cars.java
@@ -2,18 +2,45 @@ package model;
 
 import java.util.Date;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "cars")
+
 public class Cars {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "carID")
 	private int carID;
+	@Column(name="MAKE")
 	private String make;
+	@Column(name="MODEL")
 	private String model;
+	@Column(name="YEAR")
 	private String year;
+	@Column(name = "COLOR")
 	private String color;
+	@Column(name = "ManufactureDate")
 	private Date manufactureDate;
 	
 	//default constructor
 	public Cars() {
 		super();
+	}
+	
+	//full variable constructor
+	public Cars(String make, String model, String year, String color, Date manufactureDate) {
+		this.make=make;
+		this.model = model;
+		this.year = year;
+		this.color = color;
+		this.manufactureDate = manufactureDate;
 	}
 	
 	//getters and setters
@@ -33,5 +60,28 @@ public class Cars {
 		this.make = make;
 	}
 	
+	private String getCarModel() {
+		return this.model;
+	}
+	
+	private void setCarModel() {
+		this.model = model;
+	}
+	
+	private String getCarYear() {
+		return this.year;
+	}
+	
+	private void setCarYear(String year) {
+		this.year = year;
+	}
+	
+	private String getCarColor() {
+		return this.color;
+	}
+	
+	private void setCarColor(String color) {
+		this.color = color;
+	}
 	
 }
